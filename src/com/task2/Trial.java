cbf45c6a8a46e4e7931d760fc2fbb791d737edd7
Anton Vasilevsky <q3antonio@gmail.com>
2023-01-10 12:32:59 +0300

    classes created

---
+++ src/com/task2/Trial.java
@@ -0,0 +1,51 @@
+package com.task2;
+
+public class Trial {
+    private String name;
+    private int markOne;
+    private int markTwo;
+
+    private final int MARK_TO_PASS = 33;
+
+    public Trial() {
+    }
+
+    public Trial(String name, int markOne, int markTwo) {
+        this.name = name;
+        this.markOne = markOne;
+        this.markTwo = markTwo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMarkOne() {
+        return markOne;
+    }
+
+    public void setMarkOne(int markOne) {
+        this.markOne = markOne;
+    }
+
+    public int getMarkTwo() {
+        return markTwo;
+    }
+
+    public void setMarkTwo(int markTwo) {
+        this.markTwo = markTwo;
+    }
+
+    @Override
+    public String toString() {
+        return "Trial{" +
+                "name='" + name + '\'' +
+                ", markOne=" + markOne +
+                ", markTwo=" + markTwo +
+                '}';
+    }
+}
